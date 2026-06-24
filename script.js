c3ff9d27c23a9182e530acb225a274da7142cfea
flipdecorx-eng <flipdecorx@gmail.com>
2026-06-24 10:31:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function toggleMenu(){
-
 let menu = document.getElementById("plusMenu");
 
 if(menu.style.display === "block"){
-menu.style.display = "none";
+    menu.style.display = "none";
 }else{
-menu.style.display = "block";
+    menu.style.display = "block";
+}
 }
 
+function openMenu(){
+let menu = document.getElementById("sideMenu");
+
+if(menu.style.left === "0px"){
+    menu.style.left = "-260px";
+}else{
+    menu.style.left = "0px";
+}
 }
